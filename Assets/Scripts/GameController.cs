4495820b47049dc55d57f1503ca97e2cf9f2f606
Gary Grumbley <ggrumbley@trackforce.com>
2018-07-05 15:53:09 -0700

    Add room exits array to room object

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text displayText;
+
+    [HideInInspector] public RoomNavigation roomNavigation;
+    [HideInInspector] public List<string> interactionDescriptionsInRoom = new List<string>();
+
+    List<string> actionLog = new List<string>();
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        roomNavigation = GetComponent<RoomNavigation>();
+    }
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        DisplayRoomText();
+        DisplayLoggedText();
+    }
+
+    public void DisplayLoggedText()
+    {
+        string logAsText = string.Join("\n", actionLog.ToArray());
+
+        displayText.text = logAsText;
+    }
+
+    public void DisplayRoomText()
+    {
+        UnpackRoom();
+
+        string joinedInteractionDescriptions = string.Join("\n", interactionDescriptionsInRoom.ToArray());
+
+        string combinedText = roomNavigation.currentRoom.description + "\n" + joinedInteractionDescriptions;
+
+        LogStringWithReturn(combinedText);
+    }
+
+    public void LogStringWithReturn(string stringToAdd)
+    {
+        actionLog.Add(stringToAdd + "\n");
+    }
+
+    void UnpackRoom()
+    {
+        roomNavigation.UnpackExitsInRoom();
+    }
+}
---
+++ Assets/Scripts/RoomNavigation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNavigation : MonoBehaviour
+{
+    public Room currentRoom;
+
+    GameController controller;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        controller = GetComponent<GameController>();
+    }
+
+    public void UnpackExitsInRoom()
+    {
+        for (int i = 0; i < currentRoom.exits.Length; i++)
+        {
+            controller.interactionDescriptionsInRoom.Add(currentRoom.exits[i].exitDescription);
+        }
+    }
+}
