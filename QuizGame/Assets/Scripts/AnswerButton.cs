68939f7577e15dfa5e2a2f98bd67fe220fa98c32
Gary Grumbley <ggrumbley@trackforce.com>
2018-07-20 13:50:32 -0700

    Add nicer assets

--- QuizGame/Assets/Scripts/AnswerButton.cs
+++ QuizGame/Assets/Scripts/AnswerButton.cs
@@ -8,22 +8,23 @@ public class AnswerButton : MonoBehaviour
     public Text AnswerText;
 
     private AnswerData answerData;
+    private GameController gameController;
 
 
     void Start()
     {
-
-    }
-
-    void Update()
-    {
-
+        gameController = FindObjectOfType<GameController>();
     }
 
     public void Setup(AnswerData data)
     {
         answerData = data;
         AnswerText.text = answerData.answerText;
+    }
+
+    public void HandleClick()
+    {
+        gameController.AnswerButtonClicked(answerData.isCorrect);
     }
 
 }
--- QuizGame/Assets/Scripts/DataController.cs
+++ QuizGame/Assets/Scripts/DataController.cs
@@ -7,10 +7,14 @@ public class DataController : MonoBehaviour
 {
     public RoundData[] allRoundData;
 
+    private PlayerProgress playerProgress;
+
     // Use this for initialization
     void Start ()
     {
         DontDestroyOnLoad(gameObject);
+
+        LoadPlayerProgress();
 
         SceneManager.LoadScene("MenuScreen");
     }
@@ -20,9 +24,33 @@ public class DataController : MonoBehaviour
         return allRoundData[0];
     }
 
-    // Update is called once per frame
-    void Update ()
+    public void SubmitNewPlayerScore(int newScore)
     {
+        if (newScore > playerProgress.highestScore)
+        {
+            playerProgress.highestScore = newScore;
 
+            SavePlayerProgress();
+        }
+    }
+
+    public int GetHighestPlayerScore()
+    {
+        return playerProgress.highestScore;
+    }
+
+    private void LoadPlayerProgress()
+    {
+        playerProgress = new PlayerProgress();
+
+        if (PlayerPrefs.HasKey("highestScore"))
+        {
+            playerProgress.highestScore = PlayerPrefs.GetInt("highestScore");
+        }
+    }
+
+    private  void SavePlayerProgress()
+    {
+        PlayerPrefs.SetInt("highestScore", playerProgress.highestScore);
     }
 }
