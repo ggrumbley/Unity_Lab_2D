45c0ad0a55b1e26bc406371c57c6e2a7bc1db705
Gary Grumbley <ggrumbley@gmail.com>
2018-07-19 20:37:33 -0700

    Add game UI and answer button object pool

---
+++ QuizGame/Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+    public Text AnswerText;
+
+    private AnswerData answerData;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Setup(AnswerData data)
+    {
+        answerData = data;
+        AnswerText.text = answerData.answerText;
+    }
+
+}
